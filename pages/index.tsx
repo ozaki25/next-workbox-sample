ed4012c213592e0231facbedf336b6433380744d
ozaki25 <fs65ugumori@yahoo.co.jp>
2021-04-01 01:45:41 +0900

    add tailwind table layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,17 +12,30 @@ export default function Home({ skills }: Props) {
         <title>Skill List</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="container mx-auto p-4">
-        <table>
-          {skills.map(({ id, name, count1, count2, count3, count4 }) => (
-            <tr key={id}>
-              <td>{name}</td>
-              <td>{count1}</td>
-              <td>{count2}</td>
-              <td>{count3}</td>
-              <td>{count4}</td>
+      <main className="container mx-auto sm:py-4">
+        <table className="w-full bg-white shadow-md">
+          <thead>
+            <tr className="bg-gray-200 text-gray-600 text-sm">
+              <th className="py-3 sm:px-6">Name</th>
+              <th className="py-3 sm:px-6 max-w-40">Count1</th>
+              <th className="py-3 sm:px-6 max-w-40">Count2</th>
+              <th className="py-3 sm:px-6 max-w-40">Count3</th>
+              <th className="py-3 sm:px-6 max-w-40">Count4</th>
             </tr>
-          ))}
+          </thead>
+          <tbody>
+            {skills.map(({ id, name, count1, count2, count3, count4 }) => (
+              <tr key={id} className="text-gray-600 text-sm">
+                <td className="py-3 px-6 text-center whitespace-nowrap">
+                  {name}
+                </td>
+                <td className="py-3 px-6 text-right">{count1}</td>
+                <td className="py-3 px-6 text-right">{count2}</td>
+                <td className="py-3 px-6 text-right">{count3}</td>
+                <td className="py-3 px-6 text-right">{count4}</td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </main>
     </div>
